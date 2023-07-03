0affc31380433125eb036aab386f97677056794d
Jordi <im.jordiparadelo@gmail.com>
2023-07-03 13:13:53 +0200

    launch project

---
+++ components/Navbar.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import Link from "next/link";
+// Contstants
+import { NavLinks } from "@/constants";
+// Components
+import {AuthProviders} from "@/components"
+
+const Navbar = () => {
+    const session = {}
+	return (
+		<nav className='flexBetween navbar'>
+			<div className='flex-1 flexStart gap-10'>
+				<Link href='/'>
+					<Image
+						src='/logo.svg'
+						width={115}
+						height={43}
+						alt='Flexibble'
+					/>
+				</Link>
+
+				<ul className='xl:flex hidden text-small gap-7'>
+					{NavLinks.map((link) => (
+						<li key={link.key}>
+							<Link href={link.href}>{link.text}</Link>
+						</li>
+					))}
+				</ul>
+
+                <div className="flexCenter gap-4">
+                    {
+                        session ? (
+                            <>
+                            UserProfile
+                            <Link href="/create-project">Share Work</Link>
+                            </>
+                        ) : (
+                            <AuthProviders/>
+                        )
+                    }
+                    
+                </div>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
